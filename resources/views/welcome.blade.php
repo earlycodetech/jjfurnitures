0f80dab68b47f0df00c3a790e1b7e998e1b360be
Abdulhameed-20 <abdulhameedajenifuja@gmail.com>
2024-11-16 14:36:42 +0100

    Updated the gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,83 +56,32 @@
             </p>
 
             <div class="row mb-5">
-                <div class="col-lg-3 col-md-6 mb-5">
-                    <div class="card product_card h-100">
-                        <div class="box">
-                            <img class="card-img-top" src="{{ asset('assets/images/img-1.jpg') }}" alt="Title" />
-                            <span class="category">Category</span>
-                        </div>
-                        <div class="card-body">
-                            <h4 class="card-title">Living Room Sofa</h4>
-                            <p class="card-text">
-                                $ 29.9
-                            </p>
+                @forelse ($products as $product)
+                    <div class="col-lg-3 col-md-6 mb-5">
+                        <div class="card product_card">
+                            <div class="box">
+                                <img class="card-img-top" src="{{ asset('uploads/' . $product->image) }}" alt="Title" />
+                                <span class="category"> {{ $product->category->name }} </span>
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title"> {{ $product->name }} </h4>
+                                <p class="card-text">
+                                    ₦ {{ number_format($product->price, 2) }}
+                                </p>
 
-                             <div class="mt-3">
-                                <a href="#" class="btn btn-warning btn-sm">
-                                    Buy Now
-                                </a>
+                                <div class="mt-3">
+                                    <a href="" class="btn btn-warning btn-sm">
+                                        View
+                                    </a>
+
+                                    
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-5">
-                    <div class="card product_card h-100">
-                        <div class="box">
-                            <img class="card-img-top" src="{{ asset('assets/images/img-2.jpg') }}" alt="Title" />
-                            <span class="category">Category</span>
-                        </div>
-                        <div class="card-body">
-                            <h4 class="card-title">Living Room Sofa</h4>
-                            <p class="card-text">
-                                $ 29.9
-                            </p>
-                             <div class="mt-3">
-                                <a href="#" class="btn btn-warning btn-sm">
-                                    Buy Now
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-5">
-                    <div class="card product_card h-100">
-                        <div class="box">
-                            <img class="card-img-top" src="{{ asset('assets/images/img-3.jpg') }}" alt="Title" />
-                            <span class="category">Category</span>
-                        </div>
-                        <div class="card-body">
-                            <h4 class="card-title">Living Room Sofa</h4>
-                            <p class="card-text">
-                                $ 29.9
-                            </p>
-                             <div class="mt-3">
-                                <a href="#" class="btn btn-warning btn-sm">
-                                    Buy Now
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-5">
-                    <div class="card product_card h-100">
-                        <div class="box">
-                            <img class="card-img-top" src="{{ asset('assets/images/img-4.jpg') }}" alt="Title" />
-                            <span class="category">Category</span>
-                        </div>
-                        <div class="card-body">
-                            <h4 class="card-title">Living Room Sofa</h4>
-                            <p class="card-text">
-                                $ 29.9
-                            </p>
-                             <div class="mt-3">
-                                <a href="#" class="btn btn-warning btn-sm">
-                                    Buy Now
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @empty
+                    <p class="h1">No Product Added Yet</p>
+                @endforelse
             </div>
         </div>
     </section>

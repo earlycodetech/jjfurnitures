7d5f6246d51e8e8d21a27145a02f803cf5a53ef9
Abdulhameed-20 <abdulhameedajenifuja@gmail.com>
2024-11-16 15:49:16 +0100

    Cart Migration Added

---
+++ app/Models/Cart.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'amount',
+        'status',
+        'address',
+        'delivery_date',
+        'ref'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
